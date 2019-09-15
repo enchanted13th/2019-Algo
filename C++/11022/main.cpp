7ce287d0163270bccf433b53ae2dcf521d063b66
ine <enchanted13th@gmail.com>
2019-09-15 20:59:56 +0900

    ine

--- C++/11022/main.cpp
+++ C++/11022/main.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int testCnt;
+    cin >> testCnt;
+    for (int i = 0 ; i < testCnt; i++) {
+        int a, b, c;
+        cin >> a >> b;
+        cout << "Case #" << (i + 1) << ": " << a << " + " << b << " = " << (a + b) << endl;
+    }
     return 0;
 }

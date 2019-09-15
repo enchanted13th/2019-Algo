cd8d81d60942ea0597fcf4bf9847289f5a17a0e0
ine <enchanted13th@gmail.com>
2019-09-15 20:48:17 +0900

    ine

---
+++ 11022/cmake-build-debug/CMakeFiles/11022.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/11022.dir/main.cpp.o"
+  "11022.pdb"
+  "11022"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/11022.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
